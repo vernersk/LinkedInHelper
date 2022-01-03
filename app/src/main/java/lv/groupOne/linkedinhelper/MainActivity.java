609605104e7b36c20547a63acdbdd0ec28440977
vernersk <verners.kviesis@gmail.com>
2022-01-03 22:07:50 +0200

    Debug, without login / signup

--- app/src/main/java/lv/groupOne/linkedinhelper/MainActivity.java
+++ app/src/main/java/lv/groupOne/linkedinhelper/MainActivity.java
@@ -4,13 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-
 public class MainActivity extends AppCompatActivity{
+
+    boolean debug = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, AuthActivity.class));
-        setContentView(R.layout.activity_auth);
+        if(debug){
+            startActivity(new Intent(this, PrimaryActivity.class));
+            setContentView(R.layout.activity_primary);
+        }else{
+            startActivity(new Intent(this, AuthActivity.class));
+            setContentView(R.layout.activity_auth);
+        }
     }
 }
